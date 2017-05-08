3b2ab77253b4d492ed5f2b201aa0ec5ac4ef9c4c
orkun <soyluorkun@gmail.com>
2017-05-08 17:48:52 +0300

    App Fixes

--- BUFFBeacon/BUFFBeacon/ViewController.swift
+++ BUFFBeacon/BUFFBeacon/ViewController.swift
@@ -11,190 +11,187 @@ import CoreBluetooth
 import CoreLocation
 
 class ViewController: UIViewController,CBCentralManagerDelegate,CBPeripheralDelegate,CLLocationManagerDelegate{
-    @IBOutlet weak var button : UIButton!
+
+@IBOutlet weak var messageLabel: UITextView!
+@IBOutlet weak var currentRSSILabel: UILabel!
+@IBOutlet weak var currentSpeedLabel: UILabel!
+@IBOutlet weak var currentHeadingLabel: UILabel!
+@IBOutlet weak var connectionStatusLabel: UILabel!
+
+fileprivate var manager: CBCentralManager?
+fileprivate var peripheralBLE: CBPeripheral?
+
+var serviceUUID = CBUUID(string: "0000FFE3-0000-1000-8000-00805F9B34FB")
+var characteristicUUID = CBUUID(string: "0000FFE1-0000-1000-8000-00805F9B34FB")
+
+var characteristicBUFF : CBCharacteristic?
+
+var locationManager = CLLocationManager()
+var headingValue : Double? = nil
+
+var timer1 = Timer()
+
+var headingDataString : String?
+
+var speedArray = [Double?](repeating:nil,count:10)
+var averageSpeedString : String!
+var userSpeed : Double!
+
+override func viewDidLoad() {
+    super.viewDidLoad()
+    writeValueTimer()
+    manager = CBCentralManager(delegate: self, queue: nil)
+    locationManager.requestAlwaysAuthorization()
+    locationManager.requestWhenInUseAuthorization()
     
-    fileprivate var manager: CBCentralManager?
-    fileprivate var peripheralBLE: CBPeripheral?
+}
+
+func centralManagerDidUpdateState(_ central: CBCentralManager) {
+    if central.state == CBManagerState.poweredOn {
+        central.scanForPeripherals(withServices: [serviceUUID], options: nil)
+        connectionStatusLabel.text = "Searching for Device..."
+    } else {
+        connectionStatusLabel.text = "Turn On Bluetooth"
+    }
+}
+
+func centralManager(
+    _ central: CBCentralManager,
+    didDiscover peripheral: CBPeripheral,
+    advertisementData: [String : Any],
+    rssi RSSI: NSNumber) {
+    if (peripheral.name == "BUFFBeacon" && (self.peripheralBLE == nil) || (self.peripheralBLE?.state == CBPeripheralState.disconnected)) {
+        self.peripheralBLE = peripheral
+        central.connect(peripheral, options: nil)
+    }
+}
+
+func centralManager(_
+    central: CBCentralManager,
+                    didConnect peripheral: CBPeripheral) {
+    self.peripheralBLE?.delegate = self
+    peripheral.discoverServices([serviceUUID])
     
-    var serviceUUID = CBUUID(string: "0000FFE3-0000-1000-8000-00805F9B34FB")
-    var characteristicUUID = CBUUID(string: "0000FFE1-0000-1000-8000-00805F9B34FB")
-    
-    var characteristicBUFF : CBCharacteristic?
-    
-    var locationManager = CLLocationManager()
-    var headingValue : Double? = nil
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        writeValueTimer()
-        manager = CBCentralManager(delegate: self, queue: nil)
-        locationManager.requestAlwaysAuthorization()
-        locationManager.requestWhenInUseAuthorization()
+}
+
+func peripheral(
+    _ peripheral: CBPeripheral,
+    didDiscoverServices error: Error?) {
+    for service in peripheral.services! {
+        let thisService = service as CBService
         
-    }
-    
-    func centralManagerDidUpdateState(_ central: CBCentralManager) {
-        if central.state == CBManagerState.poweredOn {
-            central.scanForPeripherals(withServices: [serviceUUID], options: nil)
-        } else {
-            print("Bluetooth not available.")
-        }
-    }
-    
-    func centralManager(
-        _ central: CBCentralManager,
-        didDiscover peripheral: CBPeripheral,
-        advertisementData: [String : Any],
-        rssi RSSI: NSNumber) {
-        if (peripheral.name == "BUFFBeacon" && (self.peripheralBLE == nil) || (self.peripheralBLE?.state == CBPeripheralState.disconnected)) {
-            self.peripheralBLE = peripheral
-            central.connect(peripheral, options: nil)
-        }
-    }
-    
-    func centralManager(_
-        central: CBCentralManager,
-                        didConnect peripheral: CBPeripheral) {
-        self.peripheralBLE?.delegate = self
-        peripheral.discoverServices([serviceUUID])
-        
-    }
-    
-    func peripheral(
-        _ peripheral: CBPeripheral,
-        didDiscoverServices error: Error?) {
-        for service in peripheral.services! {
-            let thisService = service as CBService
+        if service.uuid == serviceUUID {
+            peripheral.discoverCharacteristics(
+                [characteristicUUID],
+                for: thisService
+            )
             
-            if service.uuid == serviceUUID {
-                peripheral.discoverCharacteristics(
-                    [characteristicUUID],
-                    for: thisService
-                )
+        }
+    }
+}
+
+func peripheral(
+    _ peripheral: CBPeripheral,
+    didDiscoverCharacteristicsFor service: CBService,
+    error: Error?) {
+    for characteristic in service.characteristics! {
+        let thisCharacteristic = characteristic as CBCharacteristic
+        
+        if thisCharacteristic.uuid == characteristicUUID  {
+            self.peripheralBLE?.setNotifyValue(
+                true,
+                for: thisCharacteristic
+            )
+            characteristicBUFF = thisCharacteristic
+            connectionStatusLabel.text = peripheralBLE?.name!
+            peripheralBLE?.setNotifyValue(true, for: thisCharacteristic)
+        }
+    }
+}
+
+func centralManager(
+    _ central: CBCentralManager,
+    didDisconnectPeripheral peripheral: CBPeripheral,
+    error: Error?) {
+    central.scanForPeripherals(withServices: nil, options: nil)
+    connectionStatusLabel.text = "Searching for Device..."
+}
+
+
+//    func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
+//
+//        if let dataBytes = characteristic.value {
+//            let backToString = String(data: dataBytes, encoding: String.Encoding.utf8) as String!
+//            print(dataBytes)
+//            print(backToString!)
+//            messageLabel.text = messageLabel.text + "\n"
+//            messageLabel.text = messageLabel.text + backToString!
+//        }
+//    }
+
+
+
+
+func writeValue(){
+    if peripheralBLE != nil && characteristicBUFF != nil{
+        //peripheralBLE?.readRSSI()
+        locationManager.delegate = self as CLLocationManagerDelegate
+        locationManager.desiredAccuracy = kCLLocationAccuracyBestForNavigation
+        if(CLLocationManager.headingAvailable() && CLLocationManager.locationServicesEnabled()){
+            locationManager.headingFilter = 1
+            locationManager.startUpdatingHeading()
+            locationManager.startUpdatingLocation()
+            if(headingDataString != nil && userSpeed != nil){
+                currentHeadingLabel.text = headingDataString!
+                if userSpeed >= 0.0 && userSpeed <= 7.0{
+                    currentSpeedLabel.text = String(userSpeed)
+                }
+                var isStopped : String?
+                if userSpeed == 0 {
+                    isStopped = "stopped"
+                } else{
+                    isStopped = "moving"
+                }
+                let finalDataString :String?
+                finalDataString = headingDataString! + "_" + String(describing: userSpeed!) + "/" + isStopped! + "\n"
+                self.peripheralBLE?.writeValue((finalDataString?.data(using: String.Encoding.utf8)!)!, for: characteristicBUFF!, type: CBCharacteristicWriteType.withoutResponse)
                 
             }
-        }
-    }
-    
-    func peripheral(
-        _ peripheral: CBPeripheral,
-        didDiscoverCharacteristicsFor service: CBService,
-        error: Error?) {
-        for characteristic in service.characteristics! {
-            let thisCharacteristic = characteristic as CBCharacteristic
             
-            if thisCharacteristic.uuid == characteristicUUID  {
-                self.peripheralBLE?.setNotifyValue(
-                    true,
-                    for: thisCharacteristic
-                )
-                characteristicBUFF = thisCharacteristic
-            }
+            
         }
     }
-    
-    func centralManager(
-        _ central: CBCentralManager,
-        didDisconnectPeripheral peripheral: CBPeripheral,
-        error: Error?) {
-        central.scanForPeripherals(withServices: nil, options: nil)
-    }
-    
-    var headingDataString : String?
-    var speed : Double?
-    
-    var speedArray = [Double?](repeating:nil,count:10)
-    var averageSpeedString : String!
-    var userSpeed : Double?
-    
-    func writeValue(){
-        if peripheralBLE != nil && characteristicBUFF != nil{
-        print("Connected to " + (peripheralBLE?.name)!)
-        locationManager.delegate = self as CLLocationManagerDelegate
-            locationManager.desiredAccuracy = kCLLocationAccuracyBestForNavigation
-            if(CLLocationManager.headingAvailable() && CLLocationManager.locationServicesEnabled()){
-                locationManager.headingFilter = 1
-                locationManager.startUpdatingHeading()
-                locationManager.startUpdatingLocation()
-                //if speedArray[9] != nil {
-                    //let averageSpeed = determineAverageSpeed(tempSpeedArray: speedArray)
-                    //averageSpeedString = String(averageSpeed)
-                    if(headingDataString != nil && userSpeed != nil){
-                        let finalDataString :String?
-                        finalDataString = headingDataString! + "_" + String(describing: userSpeed) + "\n"
-                        self.peripheralBLE?.writeValue((finalDataString?.data(using: String.Encoding.utf8)!)!, for: characteristicBUFF!, type: CBCharacteristicWriteType.withoutResponse)
-                    }
-                    
-                //}
-            }
-        }
-    }
-    func locationUpdate(){
-        locationManager.startUpdatingLocation()
-    }
-    
-    
-    func determineAverageSpeed(tempSpeedArray : [Double?]) -> Double{
-        var count = 0.0
-        var totalToAverage = 0.0
-        
-        for i in 0...tempSpeedArray.count-1{
-            if tempSpeedArray[i]! < 7.0 && tempSpeedArray[i]! >= 0 {
-                totalToAverage += tempSpeedArray[i]!
-                count += 1
-            }
-        }
-        speed = totalToAverage/count
-        print("Average Speed : " + String(describing: speed))
-        return speed!
-    }
-    
-    
-    
-    //Heading Update
-    func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
-            headingDataString = String(newHeading.magneticHeading)
-            print("didUpdateHeading" + headingDataString!)
-        
-    }
-    
-    //Location Update
-    var index = 0
-    
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        let userLocation : CLLocation = locations[0] as CLLocation!
-        if userLocation.timestamp .timeIntervalSinceNow > -10.0 {
-            userSpeed = Double(userLocation.speed)
-            if index < 10 {
-                print("didUpdateLocation array not full" + String(index))
-                speedArray[index] = userSpeed
-                index += 1
-            } else{
-                var i = 1
-                while i<10 {
-                    speedArray[i-1] = speedArray[i]
-                    if i == 9 {
-                        speedArray[i] = userSpeed
-                    }
-                    i += 1
-                }
-                print("didUpdateLocation array full" + String(describing: userSpeed))
-            }
+}
 
-        }
-        
-    }
+
+func peripheral(_ peripheral: CBPeripheral, didReadRSSI RSSI: NSNumber, error: Error?) {
+    currentRSSILabel.text = String(describing:RSSI)
+}
+//Heading Update
+func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
+    let roundedHeading = Double(round(100*newHeading.magneticHeading)/100)
+    headingDataString = String(roundedHeading)
     
     
-    //Timer Functions
-    var timer1 = Timer()
-    var timer2 = Timer()
-    func writeValueTimer(){
-        timer1 = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.writeValue), userInfo: nil, repeats: true)
+}
+
+//Location Update
+
+func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    let userLocation : CLLocation = locations.last as CLLocation!
+    let locationTime = userLocation.timestamp
+    if locationTime.timeIntervalSinceNow < 2.0{
+        userSpeed = Double(round(100*userLocation.speed)/100)
     }
-    
-    func locationUpdateTimer(){
-        timer2 = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(self.locationUpdate), userInfo: nil, repeats: true)
-    }
+}
+
+
+//Timer Function
+
+func writeValueTimer(){
+    timer1 = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(self.writeValue), userInfo: nil, repeats: true)
+}
+
+
 }
 
